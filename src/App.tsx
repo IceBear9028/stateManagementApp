7fa84c1760336a26bc0df606c0a31925b222f670
IceBear9028 <tlatms9028@gmail.com>
2024-02-05 21:49:29 +0900

    Init Commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { useCountStore } from './zustand/countState.ts';
+
+
+function App() {
+  const { count, increment, decrement } = useCountStore(state => state);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <h1>Zustand + React Query</h1>
+      <div className="card">
+        <p>{count} 결과값</p>
+        <button onClick={increment}>
+          Plus
+        </button>
+        <button onClick={decrement}>
+          Minus
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
