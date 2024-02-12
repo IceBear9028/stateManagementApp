6847f508822df504382a57823c45d3dacfbb1a42
IceBear9028 <tlatms9028@gmail.com>
2024-02-12 23:43:36 +0900

    React Query Devtools 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import './App.css'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools';
 import FetchZustand from './pages/fetchZustand.tsx';
 import FetchQuery from './pages/FetchQuery.tsx';
 import ActionZustand from './pages/ActionZustand.tsx';
@@ -13,9 +11,10 @@ const queryClient = new QueryClient()
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
       <h1>Vite + React</h1>
       <h1>Zustand + React Query</h1>
-      {/*<FetchQuery />*/}
+      <FetchQuery />
       <ActionZustand/>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
